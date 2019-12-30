2735c0054be2c6c9658776e79f3f0b95ac2dea95
Weiyishijie <wysj@weiyishijie.com>
2019-12-30 13:33:37 +0800

    country

---
+++ countries.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO countries (iso2, name, nicename, iso3, numcode, phonecode, name_zh, nicename_zh) VALUES
+
+('GB', 'UNITED KINGDOM', 'United Kingdom', 'GBR', 826, 44, '英国', '英国'),
+('US', 'UNITED STATES', 'United States', 'USA', 840, 1, '美国', '美国'),
+('ZM', 'ZAMBIA', 'Zambia', 'ZMB', 894, 260, '赞比亚', '赞比亚'),
+('CN', 'CHINA', 'China', 'CHN', 156, 86, '中国', '中国');
