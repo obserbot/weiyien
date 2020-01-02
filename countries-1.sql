46d0b7d462b6908c63ee303320428720fe4a469b
Weiyishijie <wysj@weiyishijie.com>
2020-01-02 22:33:23 +0800

    refine

--- countries-1.sql
+++ countries-1.sql
@@ -5,3 +5,14 @@ INSERT INTO countries (iso2, name, nicename, iso3, numcode, phonecode, name_zh, 
 ('US', 'UNITED STATES', 'United States', 'USA', 840, 1, '美国', '美国'),
 ('ZM', 'ZAMBIA', 'Zambia', 'ZMB', 894, 260, '赞比亚', '赞比亚'),
 ('CN', 'CHINA', 'China', 'CHN', 156, 86, '中国', '中国');
+
+('PH', 'PHILIPPINES', 'Philippines', 'PHL', 608, 63, '菲律宾', '菲律宾'),
+('CA', 'CANADA', 'Canada', 'CAN', 124, 1, '加拿大', '加拿大'),
+('AU', 'AUSTRALIA', 'Australia', 'AUS', 36, 61, '澳大利亚', '澳大利亚'),
+('NZ', 'NEW ZEALAND', 'New Zealand', 'NZL', 554, 64, '新西兰', '新西兰');
+
+('NG', 'NIGERIA', 'Nigeria', 'NGA', 566, 234, '尼日利亚', '尼日利亚'),
+('ZA', 'SOUTH AFRICA', 'South Africa', 'ZAF', 710, 27, '南非', '南非'),
+('JP', 'JAPAN', 'Japan', 'JPN', 392, 81, '日本', '日本'),
+('TH', 'THAILAND', 'Thailand', 'THA', 764, 66, '泰国', '泰国');
+
