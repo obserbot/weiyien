e371b777dbb0e847a80ab816f17c6d68a4fcf607
Weiyishijie <wysj@weiyishijie.com>
2020-01-13 16:18:32 +0800

    Hamburger menu

---
+++ public/assets/js/weiyi.js
@@ -0,0 +1,32 @@
+// console.log('From weiyi.js')
+//
+
+
+// Mobile hamburger menu
+
+document.getElementById('nav-toggle').addEventListener('click', toggleNav)
+
+function toggleNav() {
+  const burger = document.getElementById('nav-toggle')
+  const mobile_menu = document.getElementById("mobileMenuWrap")
+
+  /*
+  const nav = document.getElementById("navbarMenu")
+  const className = nav.getAttribute("class")
+  if (className == "navbar-menu") {
+  */
+
+  className = mobile_menu.getAttribute('class')
+  if (className == 'navbar-menu is-active level-right is-hidden') {
+    //nav.className = "navbar-menu is-active"
+    mobile_menu.className = "navbar-menu is-active level-right"
+    burger.className = 'navbar-burger burger is-active'
+    console.log('active')
+  }
+  else {
+    //nav.className = "navbar-menu"
+    mobile_menu.className = "navbar-menu is-active level-right is-hidden"
+    burger.className = 'navbar-burger burger'
+    console.log('not active')
+  }
+}
